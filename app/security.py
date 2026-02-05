7ba856bf2a6864a17ba21ae1959a3a1734835b8c
SANJAY-aitech <sanjay06.ai@gmail.com>
2026-02-05 19:12:46 +0530

    Updated security file

--- app/security.py
+++ app/security.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException
 from .config import API_KEY
 
-def verify_api_key(x_api_key: str = Header(...)):
+def validate_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
         raise HTTPException(
             status_code=401,
